e338eecaa556fc123c955ae6b4ee9a63e50028c5
Malte Richert <103412886+MalteRichert@users.noreply.github.com>
2023-07-14 08:55:31 +0200

    ci(commitlint): increase body line length limit

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   rules: {
     'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 500],
+    'header-max-length': [2, 'always', 140],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [
       2,
