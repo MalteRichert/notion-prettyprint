0c2787d0fbe501562859ea0e0d40dfce005153e4
Malte Richert <103412886+MalteRichert@users.noreply.github.com>
2023-07-23 21:55:41 +0200

    feat: migrate to tex export
    * chore: amend .gitignore with TeX config files
    
    * feat!: migrate from md to TeX
    
    - everything that previously worked in Markdown should now work in LaTex except the following:
    - longer text with background color causes unintended behavior
    - nested blocks are displayed on top level (except first level indented bullets)
    
    * ci(commitlint): update lint rules

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   rules: {
     'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [2, 'always', 500],
-    'header-max-length': [2, 'always', 140],
-    'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['start-case', 'pascal-case', 'upper-case'],
-    ],
+    'body-max-line-length': [0, 'always', 140],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', ['lower-case', 'camel-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-enum': [
@@ -27,8 +22,6 @@ module.exports = {
         'test'
       ],
     ],
-    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'signed-off-by': [2, 'always']
   }
 };
